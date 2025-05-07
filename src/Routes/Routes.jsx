962dad9dfde87c1cce421c639b65c07f0a0e6dc7
Sayed Sheikh <sayedsheikh9@gmail.com>
2025-05-07 06:36:32 +0600

    Revert "registration feature added"
    This reverts commit 0eaea42ac13e87a026ee0cdad33b3f09b9ccdd9a.

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -5,8 +5,6 @@ import Home from "../Pages/Home";
 import AppDetails from "../Pages/AppDetails";
 import AboutUs from "../Pages/AboutUs";
 import AppNotFound from "../Components/AppNotFound/AppNotFound";
-import Login from "../Pages/Login";
-import Registration from "../Pages/Registration";
 
 const router = createBrowserRouter([
   {
@@ -36,14 +34,6 @@ const router = createBrowserRouter([
       {
         path: "appNotFound",
         Component: AppNotFound,
-      },
-      {
-        path: "login",
-        Component: Login,
-      },
-      {
-        path: "signup",
-        Component: Registration,
       },
     ],
   },
